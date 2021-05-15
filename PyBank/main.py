cf022e163b23a1b29efe21a37d0816cfc7e4bc1c
RachelNewberry <rachellnewbery@yahoo.com>
2021-05-15 16:37:31 -0400

    Revisions

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,14 +6,13 @@ PyBank = os.path.join(r"C:\Users\rache\OneDrive\Documents\School\John Hopkins\Se
 
 #Revenue Variables
 Total_Months = 0
+Previous_Revenue = 0
 Total_Net = 0
-Starting_Revenue = 0
 Changes_Month = []
 Changes_Revenue = []
-Greatest_Increase =[]
-Greatest_Increase_Month = 0
-Greatest_Decrease = 0
-Greatest_Decrease_Month = 0
+Greatest_Increase =["",0]
+Greatest_Decrease = ["", 9999999999999999]
+
 
 with open(PyBank) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -27,17 +26,16 @@ with open(PyBank) as csvfile:
         Total_Months = Total_Months + 1
         Total_Net = Total_Net + int(row[1])
         
-        #Changes
-        Revenue_Change = int(row[1]) - Starting_Revenue
-        Starting_Revenue = int(row[1])
+        #Changes in Revenue
+        Revenue_Change = int(row[1]) - Previous_Revenue
+        Previous_Revenue = int(row[1])
         Changes_Revenue = Changes_Revenue + [Revenue_Change]
         Changes_Month = Changes_Month = [row[0]]
 
         #Increase
-        if int(row[1]) > Greatest_Increase:
-            Greatest_Increase = inc(row[1])
-            Greatest_Increase_Month = row[0]
-
+        if  (Revenue_Change > Greatest_Increase[1]):
+            Greatest_Increase[0] = row[1]
+            Greatest_Increase[1] = Revenue_Change
        
 
         #Decrease
@@ -49,8 +47,8 @@ with open(PyBank) as csvfile:
 Revenue_Avg = sum(Changes_Revenue) / len(Changes_Revenue)
 
 #Output
-print([f'Total Months: {total_months}'])
-print([f'Total_Net: ${Total_Net}'])
+print([f'Total Months: {Total_Months}'])
+print([f'Total Net: ${Total_Net}'])
 print([f'Avg Revenue Changes: ${Revenue_Avg}'])
 print([f'Greatest Increase in Revenue: {Greatest_Increase[0]} (${Greatest_Increase[1]})'])
 print([f'Greatest Decrease in Revenue: {Greatest_Decrease[0]} (${Greatest_Decrease[1]})'])
@@ -62,8 +60,8 @@ with open(Results, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(["Financial Analysis"])
     csvwriter.writerow(["-------------------"])
-    csvwriter.writerow([f'Total Months: {total_months}'])
-    csvwriter.writerow([f'Total_Net: ${Total_Net}'])
+    csvwriter.writerow([f'Total Months: {Total_Months}'])
+    csvwriter.writerow([f'Total Net: ${Total_Net}'])
     csvwriter.writerow([f'Avg Revenue Changes: ${Revenue_Avg}'])
     csvwriter.writerow([f'Greatest Increase in Revenue: {Greatest_Increase[0]} (${Greatest_Increase[1]})'])
     csvwriter.writerow([f'Greatest Decrease in Revenue: {Greatest_Decrease[0]} (${Greatest_Decrease[1]})'])
